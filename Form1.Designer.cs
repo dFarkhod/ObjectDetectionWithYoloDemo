91df83d25ebe835d670c0eaa42e3ab84892ee98c
Farkhod <dfarkhod@gmail.com>
2020-12-13 12:18:14 +0800

    added

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ObjectDetectionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rasmniYuklashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.narsalarniAniqlashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rasmniYuklashToolStripMenuItem,
+            this.narsalarniAniqlashToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // rasmniYuklashToolStripMenuItem
+            // 
+            this.rasmniYuklashToolStripMenuItem.Name = "rasmniYuklashToolStripMenuItem";
+            this.rasmniYuklashToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rasmniYuklashToolStripMenuItem.Text = "Rasmni Yuklash...";
+            this.rasmniYuklashToolStripMenuItem.Click += new System.EventHandler(this.rasmniYuklashToolStripMenuItem_Click);
+            // 
+            // narsalarniAniqlashToolStripMenuItem
+            // 
+            this.narsalarniAniqlashToolStripMenuItem.Name = "narsalarniAniqlashToolStripMenuItem";
+            this.narsalarniAniqlashToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.narsalarniAniqlashToolStripMenuItem.Text = "Narsalarni Aniqlash";
+            this.narsalarniAniqlashToolStripMenuItem.Click += new System.EventHandler(this.narsalarniAniqlashToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rasmniYuklashToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem narsalarniAniqlashToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectDetectionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ImageFileName = string.Empty;
+        Image<Bgr, byte> ImgInput = null;
+
+        private void rasmniYuklashToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    ImageFileName = dlg.FileName;
+                    pictureBox1.Image = Image.FromFile(ImageFileName);
+                }
+            }
+        }
+
+        private void narsalarniAniqlashToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var configurationDetector = new ConfigurationDetector();
+            var config = configurationDetector.Detect();
+            using (var yoloWrapper = new YoloWrapper(config))
+            {
+                var items = yoloWrapper.Detect(ImageToByte(pictureBox1.Image));
+                if (items.Count() > 0)
+                {
+                    foreach (YoloItem item in items)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBox1.Image);
+                        Rectangle rectangle = new Rectangle(item.X, item.Y, item.Width, item.Height);
+                        ImgInput = new Image<Bgr, byte>(bmp);
+                        ImgInput.Draw(rectangle, new Bgr(0, 0, 255), 2);
+                        ImgInput.Draw($"{item.Type} ({string.Format("{0:0.0}", item.Confidence)})", 
+                            new Point(item.X - 2, item.Y - 2), 
+                            Emgu.CV.CvEnum.FontFace.HersheyDuplex, 0.5, new Bgr(Color.DarkBlue));
+                        
+                        pictureBox1.Image = ImgInput.Bitmap;
+                    }
+                }
+            }
+        }
+
+        private byte[] ImageToByte(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, ImageFormat.Jpeg);
+                return stream.ToArray();
+            }
+        }
+    }
+}
